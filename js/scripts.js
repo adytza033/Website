cae51f8b7d634959a46fdf656f0573a54eaa2b6e
Adrian <p2662151@my365.dmu.ac.uk>
2024-04-01 00:38:34 +0100

    extra pictures

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,13 @@ menuToggler.addEventListener('click', ev => {
 /* ENTERTAIMENT */
 document.addEventListener('DOMContentLoaded', () => {
     const backgroundImg = document.getElementById('backgroundImg');
-    const hexagonCount = 40; // Number of hexagons you want to create
+    const hexagonCount = 40;
 
     for (let i = 0; i < hexagonCount; i++) {
         const hexagon = document.createElement('div');
         hexagon.classList.add('hexagon');
         backgroundImg.appendChild(hexagon);
 
-        // Apply random values for the CSS variables
         hexagon.style.setProperty('--x0', randomValue() + 'vw');
         hexagon.style.setProperty('--y0', randomValue() + 'vh');
         hexagon.style.setProperty('--x25', randomValue() + 'vw');
@@ -24,7 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function randomValue() {
-        // This will provide a value between -50 and 50
         return Math.random() * 100 - 50;
     }
 });
