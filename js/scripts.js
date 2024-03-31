d4989d443ec1e1ae90937a77d4d027d42ebe3a6a
Adrian <p2662151@my365.dmu.ac.uk>
2024-03-31 23:34:41 +0100

    keyframe done

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,29 @@ menuToggler.addEventListener('click', ev => {
     menu.classList.toggle('open');
 });
 
+/* ENTERTAIMENT */
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundImg = document.getElementById('backgroundImg');
+    const hexagonCount = 40; // Number of hexagons you want to create
 
+    for (let i = 0; i < hexagonCount; i++) {
+        const hexagon = document.createElement('div');
+        hexagon.classList.add('hexagon');
+        backgroundImg.appendChild(hexagon);
 
+        // Apply random values for the CSS variables
+        hexagon.style.setProperty('--x0', randomValue() + 'vw');
+        hexagon.style.setProperty('--y0', randomValue() + 'vh');
+        hexagon.style.setProperty('--x25', randomValue() + 'vw');
+        hexagon.style.setProperty('--y25', randomValue() + 'vh');
+        hexagon.style.setProperty('--x50', randomValue() + 'vw');
+        hexagon.style.setProperty('--y50', randomValue() + 'vh');
+        hexagon.style.setProperty('--x75', randomValue() + 'vw');
+        hexagon.style.setProperty('--y75', randomValue() + 'vh');
+    }
+
+    function randomValue() {
+        // This will provide a value between -50 and 50
+        return Math.random() * 100 - 50;
+    }
+});
